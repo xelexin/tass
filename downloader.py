8cee9e2e86e1a40262cd3d2a83225256c37cd9e7
Rafał Wądołowski <rafal@rafal.net.pl>
2016-12-30 10:29:47 +0100

    firmy

--- downloader.py
+++ downloader.py
@@ -1,10 +1,7 @@
 import requests
-import json
+import sys
 # dwa typy wierzcholkow jeden to firma, drugi to czlowiek
 # krawedzie nieskierowane pokazujace
-
-download_limit = 10
-filename = 'test'
 
 
 def check_if_last_page(data):
@@ -18,10 +15,40 @@ def check_if_last_page(data):
 def download_data(page):
     url = 'https://api-v3.mojepanstwo.pl/dane/krs_podmioty.json?page=' + str(page) + '&limit=' + str(download_limit)
     response = requests.get(url)
-    return response.text
+    return response
 
 
+def download_firm():
+    dane = download_data(1)
+    # print(json.dumps(json.loads(dane), sort_keys=True, indent=2))
+    next_page = dane.json()["Links"]["next"].split("=")[1].split("&")[0]
+    last_page = dane.json()["Links"]["last"].split("=")[1].split("&")[0]
+    for i in range(1, 1001):
+        dane = download_data(i)
+        data = dane.json()["Dataobject"]
+        for j in range(0, len(data)):
+            idfirm = data[j]["id"]
+            namefirm = data[j]["data"]["krs_podmioty.firma"]
+            firms[idfirm] = namefirm
+        sys.stdout.write("\rPobieranie firm: {0}/{1}".format(i, last_page))
+        sys.stdout.flush()
+    print('')
 
 
-print(json.dumps(json.loads(download_data(10)), sort_keys=True, indent=2))
+def save_dictionary(dict, filename):
+    target = open(filename, 'w')
+    target.write(str(dict))
+    target.close()
 
+
+def read_dictionary(filename):
+    dict = open(filename, 'r').read()
+    return eval(dict)
+
+
+download_limit = 1000
+firms = {}
+#download_firm()
+# save_dictionary(firms, "firmy.dat")
+firms = read_dictionary("firmy.dat")
+print(len(firms))
