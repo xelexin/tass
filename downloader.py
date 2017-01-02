d77a897d6d8c20cf6c6ee7921c675e3f6d34a1c3
Rafał Wądołowski <rafal@rafal.net.pl>
2017-01-02 02:15:21 +0100

    multithread downloading

--- downloader.py
+++ downloader.py
@@ -1,5 +1,8 @@
 import requests
 import sys
+import json
+import _thread
+import threading
 # dwa typy wierzcholkow jeden to firma, drugi to czlowiek
 # krawedzie nieskierowane pokazujace
 
@@ -24,6 +27,12 @@ def download_person_data(page):
     return response
 
 
+def download_firm_id_data(number):
+    url = 'https://api-v3.mojepanstwo.pl/dane/krs_podmioty/'+str(number)+'.json'
+    response = requests.get(url)
+    return response
+
+
 def download_firm():
     dane = download_firm_data(1)
     # print(json.dumps(json.loads(dane), sort_keys=True, indent=2))
@@ -39,6 +48,19 @@ def download_firm():
         sys.stdout.write("\rPobieranie firm: {0}/{1}".format(i, last_page))
         sys.stdout.flush()
     print('')
+
+
+def download_firm_by_id(start, end):
+    firms = {}
+    for i in range(int(start), int(end)):
+        sys.stdout.write("\rPobieranie firm: {0}/{1}".format(i, end))
+        sys.stdout.flush()
+        dane = download_firm_id_data(i)
+        dane = dane.json()
+        if 'name' in dane:
+            continue
+        firms[i] = str(dane)
+    save_dictionary(firms, "firms_id_"+str(start)+"_"+str(end))
 
 
 def download_person():
@@ -72,13 +94,44 @@ def read_dictionary(filename):
     return eval(dict)
 
 
-download_limit = 1000
+def run_download_firm():
+    size = 800000
+    threads = 64
+    sizeofdownload = size/threads
+    print("Size of one download: "+str(sizeofdownload))
+    # sizeofdownload = 10
+    #download_firm_by_id(0,10)
+    for i in range(0, threads):
+        #thread.start_new_thread(download_firm_by_id(), (i*sizeofdownload, (i+1)*sizeofdownload))
+        try:
+            # _thread.start_new_thread(download_firm_by_id, (0*i, 10*i))
+            absc = threading.Thread(target=download_firm_by_id, args=(sizeofdownload*i, sizeofdownload*(i+1)))
+            absc.start()
+        except Exception as e:
+            import traceback
+            traceback.format_exc()
+            print(e)
+
+
+
+
+
+#download_limit = 1000
 #firms = {}
 #download_firm()
 # save_dictionary(firms, "firmy.dat")
-firms = read_dictionary("firmy.dat")
-print(len(firms))
+#firms = read_dictionary("firmy.dat")
+#print(len(firms))
+#print(max(firms.keys(), key=int))
+
+#firms = download_firm_by_id()
+#print(len(firms))
+#save_dictionary(firms, "firmyid.dat")
+run_download_firm()
+
 #persons = download_person()
 #save_dictionary(persons, "ludzie.dat")
-persons = read_dictionary("ludzie.dat")
-print(len(persons))
+#persons = read_dictionary("ludzie.dat")
+#print(len(persons))
+
+#print(max(persons.keys(), key=int))
